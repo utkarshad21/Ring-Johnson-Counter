ea7a95715d8fa8792f7acd46582fc0e7a8bceb6f
utkarshad21 <154047293+utkarshad21@users.noreply.github.com>
2023-12-29 16:11:01 +0530

    Create rj_counter_tb.v

---
+++ rj_counter_tb.v
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Creator: Utkarsha Dongarjal
+// Github channel name: utkarshad21
+// Module Name: rj_counter_tb
+// Project Name: Testbench of Ring and Johnson Counter
+//////////////////////////////////////////////////////////////////////////////////
+
+module rj_counter_tb;
+wire [3:0] q;
+reg clk;
+reg rst; 
+reg rj;
+rj_varad r1(q, clk, rst, rj);
+initial begin
+clk = 0;
+forever #20 clk = ~clk;
+end
+initial begin
+{rst, rj} = 2'b00;
+#500;
+{rst, rj} = 2'b01;
+#500;
+$finish;
+end
+endmodule
